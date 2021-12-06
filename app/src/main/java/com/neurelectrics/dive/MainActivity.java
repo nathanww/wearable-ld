f9f9b1cb31a5ae74aeff9050b82d8f88a4acde27
nathanww <nathanww@gmail.com>
2021-12-06 15:34:59 -0600

    basic code for playing sounds and interrupting on arousal

--- app/src/main/java/com/neurelectrics/dive/MainActivity.java
+++ app/src/main/java/com/neurelectrics/dive/MainActivity.java
@@ -37,6 +37,20 @@ public class MainActivity extends AppCompatActivity {
     MediaPlayer noguidance;
     Timer trainingTimer;
 
+    float oldx,oldy,oldz=0; //variables to detect sudden motion
+    float MOTION_THRESH=1f; //how much motion is considered an arousal
+    float ONSET_THRESH=0.99f; //how high does the rem probability have to be to trigger cueing?
+    float cueVolume=1.0f;
+    float CUE_VOLUME_INC=0.01f; //how much does the cue volume increase ach second?
+    boolean DEBUG_MODE=true;
+
+    boolean cueRunning=false;
+    int lastArousal=0;
+    int ONSET_TIME=14400; //minimum time the app must be running before it will cue
+    int BACKOFF_TIME=600;
+    int elapsedTime=0;
+
+
     int[] delayTimes={0,45,45,45,45,70,55,65,70,80,65,60,75,75,90,120};
     int STIMULUS_LENGTH=73;  //how long is the cue sound? THis  prevents issues with it overlapping
     int delayItem=0;
@@ -46,6 +60,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //set up the lucid music
+        lucidMusic= MediaPlayer.create(MainActivity.this,R.raw.eno1fade);
+        lucidMusic.setVolume(1.0f,1.0f);
         //start the Fitbit server
         server = new fitbitServer();
         try {
@@ -62,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
                 startButton.setEnabled(false);
                 startTraining= MediaPlayer.create(MainActivity.this,R.raw.training1);
                 training2= MediaPlayer.create(MainActivity.this,R.raw.beeps);
-                lucidMusic= MediaPlayer.create(MainActivity.this,R.raw.eno1fade);
+
                 lucidMusic.setLooping(false);
 
                 lucidMusic.setVolume(0.8f,0.8f);
@@ -87,6 +105,7 @@ public class MainActivity extends AppCompatActivity {
                 training2.setOnCompletionListener(new MediaPlayer.OnCompletionListener() { //when the first part of the instrutionas are complete, star tthe second part
                     @Override
                     public void onCompletion(MediaPlayer mp) {
+                        trainingTimer=new Timer();
                         trainingTimer.schedule(new TimerTask() {
                             @Override
                             public void run() {
@@ -98,6 +117,23 @@ public class MainActivity extends AppCompatActivity {
                 training2.start();
             }
         });
+        if (DEBUG_MODE) {
+            ONSET_TIME=0;
+            BACKOFF_TIME=10;
+            ONSET_THRESH=0;
+
+        }
+
+        //start the Fitbit monitoring
+        Timer fitbitMonitor=new Timer();
+        fitbitMonitor.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if (System.currentTimeMillis() > lastPacket+10000) {
+
+                }
+            }
+        }, 0, 10000);
     }
 
     void handleTrainingSounds() { //handle the repeating cues during training with different inter stimulus intervals
@@ -135,14 +171,66 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
+        /*dummy stage data
+        {data={"hr":77,"accx":-3.365152359008789,"accy":3.8511905670166016,"accz":8.557137489318848,"gyrox":1.1430635452270508,"gyroy":0.11505126953125,"gyroz":-0.23862648010253906,"seconds":286,"aqTime":1638816782462,"b":96}STAGE{"Probability( is3=0 )":0.9999999999999639,"Probability( is3=1 )":3.6029740452123885e-14,"Most Likely is3":0}, NanoHttpd.QUERY_STRING=data=%7B%22hr%22%3A77%2C%22accx%22%3A-3.365152359008789%2C%22accy%22%3A3.8511905670166016%2C%22accz%22%3A8.557137489318848%2C%22gyrox%22%3A1.1430635452270508%2C%22gyroy%22%3A0.11505126953125%2C%22gyroz%22%3A-0.23862648010253906%2C%22seconds%22%3A286%2C%22aqTime%22%3A1638816782462%2C%22b%22%3A96%7DSTAGE%7B%22Probability(%20is3%3D0%20)%22%3A0.9999999999999639%2C%22Probability(%20is3%3D1%20)%22%3A3.6029740452123885e-14%2C%22Most%20Likely%20is3%22%3A0%7D}*/
+
+        void handleStaging(String stageData) {
+            elapsedTime++;
+            //logic for controlling cueing
+            if (elapsedTime >= ONSET_TIME) {
+                if (stageData.indexOf("\"Probability( is3=1 )\":") > -1) {
+                    //String test=stageData.split("\"Probability( is3=1 )\":")[0];
+                    //Log.d("debug",test);
+                    float s3Prob=Float.parseFloat(stageData.split(":")[11].split(",")[0]);
+                    float motionX=Float.parseFloat(stageData.split(":")[1].split(",")[0]);
+                    float motionY=Float.parseFloat(stageData.split(":")[2].split(",")[0]);
+                    float motionZ=Float.parseFloat(stageData.split(":")[3].split(",")[0]);
+                    boolean isArousal=false;
+                    if (cueRunning && (Math.abs(motionX-oldx) > MOTION_THRESH ||Math.abs(motionY-oldy) > MOTION_THRESH || Math.abs(motionZ-oldz) > MOTION_THRESH)) {
+                        isArousal=true;
+                        lastArousal=elapsedTime;
+                    }
+                    oldx=motionX;
+                    oldy=motionY;
+                    oldz=motionZ;
+
+
+                    //cueing logic control
+                    Log.i("cuedata",cueRunning+","+s3Prob+","+(elapsedTime-lastArousal));
+                    if (s3Prob >= ONSET_THRESH && elapsedTime >= ONSET_TIME && elapsedTime-lastArousal >= BACKOFF_TIME) { //conditions are good for cueing
+                        if (!cueRunning) {
+                            cueRunning=true;
+                            lucidMusic= MediaPlayer.create(MainActivity.this,R.raw.eno1fade);
+                            lucidMusic.setVolume(cueVolume,cueVolume);
+                            lucidMusic.setLooping(true);
+                            lucidMusic.start();
+                        }
+                        cueVolume=cueVolume+CUE_VOLUME_INC;
+                    }
+                    else {
+
+                        if (cueRunning) {
+                            lucidMusic.stop();
+                            cueRunning=false;
+
+
+                        }
+                    }
+                }
+                else {
+                    Log.e("cuedata","no stage");
+                }
+            }
+        }
 
         public Response serve(String uri, Method method,
                               Map<String, String> header,
                               Map<String, String> parameters,
                               Map<String, String> files) {
-            Log.e("fitbitserver", "request");
+            //Log.e("fitbitserver", "request");
             if (uri.indexOf("rawdata") > -1) { //recieved a data packet from the Fitbit, set the Fitbit status to good.
                 Log.i("data",parameters.toString());
+                handleStaging(parameters.toString());
             }
             return newFixedLengthResponse(Response.Status.OK, "normal", "");
 
