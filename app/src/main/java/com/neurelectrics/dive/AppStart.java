fa8f3697193f3036ee93e910658b26cac1bc369d
nathanww <nathanww@gmail.com>
2022-02-13 19:52:09 -0600

    Set up trial structure and add fitbit test

--- app/src/main/java/com/neurelectrics/dive/AppStart.java
+++ app/src/main/java/com/neurelectrics/dive/AppStart.java
@@ -2,13 +2,34 @@ package com.neurelectrics.dive;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class AppStart extends AppCompatActivity {
-
+    void checkStatus() {
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        int taskStatus = sharedPref.getInt("taskStatus", 0); //get where we are in the experiment
+        if (taskStatus == 0) { //consent form has not been filled out
+            Intent getConsent = new Intent(this, ConsentActivity.class);
+            startActivity(getConsent); //consent is important
+        }
+        if (taskStatus == 1) { //consent form has not been filled out
+            Intent testFitbit = new Intent(this, FitbitTest.class);
+            startActivity(testFitbit); //consent is important
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_start);
+        checkStatus();
+    }
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        checkStatus();
     }
 }
---
+++ app/src/main/java/com/neurelectrics/dive/ConsentActivity.java
@@ -0,0 +1,43 @@
+package com.neurelectrics.dive;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class ConsentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        Log.i("consent","starting consent");
+        String pageTarget="https://docs.google.com/forms/d/e/1FAIpQLScoOcp3gFlGw6U8ais96aK71CEUjLdsno1Mbp2TbtbMSKa_jQ/viewform";
+        setContentView(R.layout.activity_consent);
+        WebView wv = (WebView) findViewById(R.id.consentView);
+        wv.loadUrl(pageTarget);
+        WebSettings webSettings = wv.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        Context con=getApplicationContext();
+        wv.setWebViewClient(new WebViewClient() {
+
+            public void onPageFinished(WebView view, String url) {
+                if (url.indexOf("formResponse") > -1) {
+                    Log.i("consenturl",url);
+                    Log.i("consent","consent complete");
+                    editor.putInt("taskStatus",1);
+                    editor.commit();
+                    finish();
+                }
+            }
+        });
+
+    }
+}
