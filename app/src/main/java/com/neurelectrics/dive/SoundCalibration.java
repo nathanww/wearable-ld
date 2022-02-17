0915b1ecc4c488d8b0d2505b5a1bffe66fd81d4a
nathanww <nathanww@gmail.com>
2022-02-17 10:57:33 -0600

    Soud calibration

---
+++ app/src/main/java/com/neurelectrics/dive/SoundCalibration.java
@@ -0,0 +1,14 @@
+package com.neurelectrics.dive;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SoundCalibration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sound_calibration);
+    }
+}
