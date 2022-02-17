df6a978f9b276d9b1bfcd0618ad4678a2c20477f
nathanww <nathanww@gmail.com>
2022-02-16 18:42:10 -0600

    Add internet connection test to consent form

--- app/src/main/java/com/neurelectrics/dive/ConsentActivity.java
+++ app/src/main/java/com/neurelectrics/dive/ConsentActivity.java
@@ -1,23 +1,61 @@
 package com.neurelectrics.dive;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Log;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
 public class ConsentActivity extends AppCompatActivity {
+    private boolean checkInternet() { //returns true if internet is connected
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        try {
+            Socket sock = new Socket();
+            sock.connect(new InetSocketAddress("8.8.8.8", 53), 1500);
+            sock.close();
+            return true;
+        } catch (IOException e) { return false; }
+    }
+
+
+    private void connectionAlert() {
+        AlertDialog.Builder builder;
+        builder = new AlertDialog.Builder(this);
+        builder.setMessage("Unable to reach the Internet. Make sure you're connected and try again.")
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                });
+
+        //Creating dialog box
+        AlertDialog alert = builder.create();
+        //Setting the title manually
+        alert.setTitle("No connection");
+        alert.show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         SharedPreferences sharedPref = getApplicationContext().getSharedPreferences("prefs", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
-
+        if (!checkInternet()) {
+            connectionAlert();
+        }
         Log.i("consent","starting consent");
         String pageTarget="https://docs.google.com/forms/d/e/1FAIpQLScoOcp3gFlGw6U8ais96aK71CEUjLdsno1Mbp2TbtbMSKa_jQ/viewform";
         setContentView(R.layout.activity_consent);
