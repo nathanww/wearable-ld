7dda0a7a9e1b5fc77397a6c0f69aaf08a4b523d9
nathanww <nathanww@gmail.com>
2022-03-14 11:54:36 -0500

    Night 7 report

---
+++ app/src/main/java/com/neurelectrics/dive/Night7.java
@@ -0,0 +1,88 @@
+package com.neurelectrics.dive;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Random;
+import java.util.function.Consumer;
+
+public class Night7 extends AppCompatActivity {
+
+    private boolean checkInternet() { //returns true if internet is connected
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        try {
+            Socket sock = new Socket();
+            sock.connect(new InetSocketAddress("8.8.8.8", 53), 1500);
+            sock.close();
+            return true;
+        } catch (IOException e) { return false; }
+    }
+
+
+    private void connectionAlert() {
+        AlertDialog.Builder builder;
+        builder = new AlertDialog.Builder(this);
+        builder.setMessage("Unable to reach the Internet. Make sure you're connected and try again.")
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                });
+
+        //Creating dialog box
+        AlertDialog alert = builder.create();
+        //Setting the title manually
+        alert.setTitle("No connection");
+        alert.show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_night7);
+        SharedPreferences sharedPref = getApplicationContext().getSharedPreferences("prefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        if (!checkInternet()) {
+            connectionAlert();
+        }
+        Log.i("night7 questionnaire","starting questionnaire");
+        //get the participant ID if it exists, if not generate a new one
+        int pid=sharedPref.getInt("pid",-1);
+        String pageTarget="https://northwestern.az1.qualtrics.com/jfe/form/SV_0rcAYVySidemymW?participantID="+pid;
+        WebView wv = (WebView) findViewById(R.id.sqView);
+        wv.loadUrl(pageTarget);
+        WebSettings webSettings = wv.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        Context con=getApplicationContext();
+        wv.setWebViewClient(new WebViewClient() {
+
+            public void onPageFinished(WebView view, String url) {
+                if (url.indexOf("google.com") > -1) {
+                    Log.i("squrl",url);
+                    Log.i("sq","complete");
+
+                    editor.putInt("taskStatus",3);
+                    editor.commit();
+                    finish();
+                }
+            }
+        });
+
+    }
+}
