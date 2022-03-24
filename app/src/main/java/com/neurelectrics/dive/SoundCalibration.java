b8d3a8820a26efe67dd6c85676e33c9152b7b892
nathanww <nathanww@gmail.com>
2022-03-24 15:47:47 -0500

    Improve instructions and new build

--- app/src/main/java/com/neurelectrics/dive/SoundCalibration.java
+++ app/src/main/java/com/neurelectrics/dive/SoundCalibration.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
@@ -30,7 +31,15 @@ public class SoundCalibration extends AppCompatActivity {
             }
         }, 10000, 1000);
     }
+    void maximizeVolume() {
+        AudioManager am =
+                (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
+        am.setStreamVolume(
+                AudioManager.STREAM_MUSIC,
+                am.getStreamMaxVolume(AudioManager.STREAM_MUSIC),
+                0);
+    }
     @Override
     public void onBackPressed() {
         // Not calling **super**, disables back button in current screen.
@@ -82,6 +91,7 @@ public class SoundCalibration extends AppCompatActivity {
             @Override
             public void onCompletion(MediaPlayer mp) {
                 soundHeard.setVisibility(View.VISIBLE);
+                maximizeVolume();
                 final Handler soundloop = new Handler();
                 soundloop.postDelayed(new Runnable() { //start playing the training sound at 10-s intervals
                     public void run() {
