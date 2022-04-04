e924877117925b5b1df1003a1ee4a4cc41fbd15c
nathanww <nathanww@gmail.com>
2022-04-04 07:47:55 -0500

    New build

--- app/src/main/java/com/neurelectrics/dive/DreamReport.java
+++ app/src/main/java/com/neurelectrics/dive/DreamReport.java
@@ -43,7 +43,7 @@ import java.util.Map;
 public class DreamReport extends AppCompatActivity {
     SharedPreferences sharedPref;
     SharedPreferences.Editor editor;
-    int APP_VERSION=8;
+    int APP_VERSION=9;
     long startedTime=0;
     @Override
     public void onBackPressed() {
