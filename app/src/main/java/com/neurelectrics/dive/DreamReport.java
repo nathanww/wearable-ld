04b39af6ca36bf4e7f4a632b7458db87512265d7
nathanww <nathanww@gmail.com>
2022-10-09 07:45:37 -0400

    Increase version number in dream report

--- app/src/main/java/com/neurelectrics/dive/DreamReport.java
+++ app/src/main/java/com/neurelectrics/dive/DreamReport.java
@@ -43,7 +43,7 @@ import java.util.Map;
 public class DreamReport extends AppCompatActivity {
     SharedPreferences sharedPref;
     SharedPreferences.Editor editor;
-    int APP_VERSION=16;
+    int APP_VERSION=17;
     long startedTime=0;
     @Override
     public void onBackPressed() {
